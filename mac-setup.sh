f47c199292696e2fcdfe2000fd7c2032690d3376
Cassandra Keddis <cassandra@thinkific.com>
2026-01-26 16:01:26 -0600

    fix(macSetup): remove redundant command

--- mac-setup.sh
+++ mac-setup.sh
@@ -27,10 +27,7 @@ defaults write com.apple.dock show-recents -bool false
 defaults write com.apple.dock wvous-br-corner -int 0
 defaults write com.apple.dock wvous-br-modifier -int 0
 
-# Prevent itunes from auto-launching when pressing play button
-launchctl unload -w /System/Library/LaunchAgents/com.apple.rcd.plist 2>/dev/null || true
-
-# Configure noTunes to hide menu bar icon
+# Configure noTunes to hide menu bar icon and allow Spotify
 defaults write digital.twisted.noTunes showMenuBarIcon -bool false
 
 # Restart Dock to apply changes
